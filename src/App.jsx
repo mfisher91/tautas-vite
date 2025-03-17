114111ccb7d1390677df928733672b3e8c3d522b
Mo Fisher <marisfisher@hotmail.co.uk>
2025-03-17 13:46:37 +0000

    Simplified homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
 import "./App.css";
-import { motion } from "motion/react";
-import { Icon } from "@mui/material";
-import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
+
+import Home from "./pages/home";
 
 function App() {
   return (
     <>
-      <div className="w-full h-full text-ghost-white flex justify-center content-center flex-wrap flex-col text-center">
-        <div className="pb-4 mb-6 border-b-4 border-solid border-ghost-white">
-          <h1 className="text-8xl">Tautas</h1>
-        </div>
-        <div className="flex w-150 justify-between text-2xl">
-          <a href="#">GIGS</a>
-          <a href="#">ABOUT</a>
-          <a href="#">MEDIA</a>
-          <a href="#">CONTACT</a>
-        </div>
-      </div>
-      <motion.div
-        className="absolute bottom-0 w-full text-center text-ghost-white"
-        animate={{
-          y: [-8, -36, -8],
-          transition: { ease: ["easeIn", "easeOut"] },
-        }}
-        transition={{ type: "spring", stiffness: 100 }}
-      >
-        <a href="#">
-          <Icon component={KeyboardDoubleArrowDownIcon} fontSize="large" />
-        </a>
-      </motion.div>
+      <Home />
     </>
   );
 }
---
+++ src/pages/home.jsx
@@ -0,0 +1,19 @@
+import "../App.css";
+
+function Home() {
+  return (
+    <>
+      <div className="hero-with-bg">
+        <div className="h-full text-ghost-white flex justify-center content-center flex-wrap bg-popping-pink">
+          <h1 className="text-8xl md:text-9xl">Tautas</h1>
+        </div>
+      </div>
+      <div className="absolute bottom-0 w-full text-center text-ghost-white">
+        <p className="font-russo">SCROLL</p>
+        <div className="border-1 border-solid border-ghost-white h-15 w-0 mx-auto my-4" />
+      </div>
+    </>
+  );
+}
+
+export default Home;
