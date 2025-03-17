03c760e539fbf2c210e5cf08bbda9e50391160a2
Mo Fisher <marisfisher@hotmail.co.uk>
2025-03-17 12:06:09 +0000

    WIP: homepage design.
    This will likely change - moving away to stop faffing

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { motion } from "motion/react";
+import { Icon } from "@mui/material";
+import KeyboardDoubleArrowDownIcon from "@mui/icons-material/KeyboardDoubleArrowDown";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full h-full text-ghost-white flex justify-center content-center flex-wrap flex-col text-center">
+        <div className="pb-4 mb-6 border-b-4 border-solid border-ghost-white">
+          <h1 className="text-8xl">Tautas</h1>
+        </div>
+        <div className="flex w-150 justify-between text-2xl">
+          <a href="#">GIGS</a>
+          <a href="#">ABOUT</a>
+          <a href="#">MEDIA</a>
+          <a href="#">CONTACT</a>
+        </div>
       </div>
-      <h1 className="text-3xl font-medium text-black">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <motion.div
+        className="absolute bottom-0 w-full text-center text-ghost-white"
+        animate={{
+          y: [-8, -36, -8],
+          transition: { ease: ["easeIn", "easeOut"] },
+        }}
+        transition={{ type: "spring", stiffness: 100 }}
+      >
+        <a href="#">
+          <Icon component={KeyboardDoubleArrowDownIcon} fontSize="large" />
+        </a>
+      </motion.div>
     </>
   );
 }
